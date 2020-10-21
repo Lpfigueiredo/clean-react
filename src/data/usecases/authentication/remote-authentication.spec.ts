654b92f3456aaa951cc16628f03cc4dacff21eb6
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2020-10-20 21:40:06 -0300

    refactor: group imports in index files

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,11 +1,10 @@
 import { RemoteAuthentication } from './remote-authentication'
-import { HttpPostClientSpy } from '../../test/mock-http-client'
-import { HttpStatusCode } from '../../protocols/http/http-response'
-import { mockAccountModel, mockAuthentication } from '../../../domain/test/mock-account'
-import { InvalidCredentialsError } from '../../../domain/errors/invalid-credentials-error'
-import { UnexpectedError } from '../../../domain/errors/unexpected-error'
-import { AuthenticationParams } from '../../../domain/usecases/authentication'
-import { AccountModel } from '../../../domain/models/account-model'
+import { HttpPostClientSpy } from '../../test'
+import { HttpStatusCode } from '../../protocols/http'
+import { mockAccountModel, mockAuthentication } from '../../../domain/test'
+import { InvalidCredentialsError, UnexpectedError } from '../../../domain/errors'
+import { AuthenticationParams } from '../../../domain/usecases'
+import { AccountModel } from '../../../domain/models'
 import faker from 'faker'
 
 type SutTypes = {
