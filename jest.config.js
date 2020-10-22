64bd448b194071042dc7e8770f5c3c71ae8b0252
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2020-10-21 22:23:58 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,14 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
