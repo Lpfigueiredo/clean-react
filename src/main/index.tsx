ccde58b718e6cd1ebd4b6fade49e1213f854954d
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2020-10-27 21:51:00 -0300

    feat: add app router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,9 +1,10 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Login } from '../presentation/pages'
+import { Router } from '../presentation/components'
+import '../presentation/styles/global.scss'
 
 ReactDOM.render(
-  <Login />,
+  <Router />,
   document.getElementById('main')
 )
