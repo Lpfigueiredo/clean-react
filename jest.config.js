754847f674cdf5c0b6d0f202d81f61616878053c
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2020-10-28 20:45:35 -0300

    chore: add testing library

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
