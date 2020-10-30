8ac4f91385828e526715603fc8c6457e5d7c390e
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2020-10-29 21:46:17 -0300

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
