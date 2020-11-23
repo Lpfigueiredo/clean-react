228927ba8c8e636b21e675d2c80ffd9dbc3a2742
Leonardo Figueiredo <leommko@gmail.com>
2020-11-22 21:49:52 -0300

    refactor: move AuthenticationSpy to its own file

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,23 +3,7 @@ import React from 'react'
 import faker from 'faker'
 import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react'
 import Login from './login'
-import { ValidationStub } from '../../test'
-import { Authentication, AuthenticationParams } from '../../../domain/usecases'
-import { AccountModel } from '../../../domain/models'
-import { mockAccountModel } from '../../../domain/test'
-
-class AuthenticationSpy implements Authentication {
-  account = mockAccountModel()
-  params: AuthenticationParams = {
-    email: '',
-    password: ''
-  }
-
-  async auth (params: AuthenticationParams): Promise<AccountModel> {
-    this.params = params
-    return Promise.resolve(this.account)
-  }
-}
+import { ValidationStub, AuthenticationSpy } from '../../test'
 
 type SutTypes = {
   sut: RenderResult
---
+++ src/presentation/test/mock-authentication.ts
@@ -0,0 +1,16 @@
+import { AccountModel } from '../../domain/models'
+import { mockAccountModel } from '../../domain/test'
+import { Authentication, AuthenticationParams } from '../../domain/usecases'
+
+export class AuthenticationSpy implements Authentication {
+  account = mockAccountModel()
+  params: AuthenticationParams = {
+    email: '',
+    password: ''
+  }
+
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
+    this.params = params
+    return Promise.resolve(this.account)
+  }
+}
