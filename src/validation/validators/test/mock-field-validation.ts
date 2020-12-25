546aac5e1c0ab97f52b357061be8aadc527d7164
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2020-12-25 14:59:06 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/test/mock-field-validation.ts
@@ -0,0 +1,15 @@
+import { FieldValidation } from '../../protocols/field-validation'
+
+export class FieldValidationSpy implements FieldValidation {
+  readonly field: string
+  error: Error | null = null
+
+  constructor(field: string) {
+    this.field = field
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  validate(value: string): Error | null {
+    return this.error
+  }
+}
