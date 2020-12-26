5c086f45d6d0dfd4ffdd3bb00dd4f7e9677f0aa0
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2020-12-26 20:50:51 -0300

    fix: change AxiosHttpClient to handle exceptions

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -5,17 +5,24 @@ import {
 } from '../../../data/protocols/http'
 import { AuthenticationParams } from '../../../domain/usecases'
 import { AccountModel } from '../../../domain/models'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 
 // eslint-disable-next-line prettier/prettier
 export class AxiosHttpClient implements HttpPostClient<AuthenticationParams, AccountModel> {
   async post(
     params: HttpPostParams<AuthenticationParams>
   ): Promise<HttpResponse<AccountModel>> {
-    const httpResponse = await axios.post(params.url, params.body)
-    return {
-      statusCode: httpResponse.status,
-      body: httpResponse.data as AccountModel
+    try {
+      const httpResponse = await axios.post(params.url, params.body)
+      return {
+        statusCode: httpResponse.status,
+        body: httpResponse.data as AccountModel
+      }
+    } catch (error) {
+      return {
+        statusCode: (error as AxiosError).response?.status || 500,
+        body: (error as AxiosError).response?.data as AccountModel
+      }
     }
   }
 }
