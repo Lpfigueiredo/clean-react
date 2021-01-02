a8965a7d155e094544efb50647aa6653fba32cbb
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2021-01-02 16:41:50 -0300

    test: ensure LocalStorageAdapter calls LocalStorage with correct values

---
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -0,0 +1,19 @@
+import { LocalStorageAdapter } from './local-storage-adapter'
+import faker from 'faker'
+import 'jest-localstorage-mock'
+
+describe('LocalStorageAdapter', () => {
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
+  test('Should call LocalStorage with correct values', async () => {
+    const sut = new LocalStorageAdapter()
+    const key = faker.database.column()
+    const value = faker.random.word()
+    await sut.set(key, value)
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
+    expect(1).toBe(1)
+  })
+})
